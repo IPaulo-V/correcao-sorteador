c7fad9049221349fb00edb12c3f8a2abf346235b
IPaulo-V <paulo.victor_sv@proton.me>
2025-10-18 22:38:34 -0300

    array e  loop(for)

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 function sortear() {
-    let quantidade = document.getElementById('quantidade').value;
-    let ate = document.getElementById('ate').value;
-    let de = document.getElementById('de').value;
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let ate = parseInt(document.getElementById('ate').value);
+    let de = parseInt(document.getElementById('de').value);
+
+    let sorteados = [];
     
+    for(let i = 0; i < quantidade; i++) {
+        let numero = obterNumeroAleatorio(de,ate);
+        sorteados.push(numero);
+    }
+    alert(sorteados);
+}
+function obterNumeroAleatorio (min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
