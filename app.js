8daf5fac42152f848959798cbd29f70737ad5337
IPaulo-V <paulo.victor_sv@proton.me>
2025-10-18 22:21:20 -0300

    definindo inputs

---
+++ app.js
@@ -0,0 +1,6 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let ate = document.getElementById('ate').value;
+    let de = document.getElementById('de').value;
+    
+}
